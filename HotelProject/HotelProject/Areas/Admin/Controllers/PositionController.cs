68124bc8af1c94dec8e19cccbf53e86ca860edfb
Reyhanhuseynova <reyhanhuseynova118@gmail.com>
2023-12-02 00:02:02 +0400

    Position Crud

--- HotelProject/HotelProject/Areas/Admin/Controllers/PositionController.cs
+++ HotelProject/HotelProject/Areas/Admin/Controllers/PositionController.cs
@@ -11,7 +11,7 @@ namespace HotelProject.Areas.Admin.Controllers
         private readonly AppDbContext _db;
         public PositionController(AppDbContext db)
         {
-            _db=db;
+            _db = db;
         }
         public async Task<IActionResult> Index()
         {
@@ -19,6 +19,92 @@ namespace HotelProject.Areas.Admin.Controllers
             return View(teams);
         }
 
-        
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Position p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            await _db.Positions.AddAsync(p);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Update(int ?id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Position ?dbposition=await _db.Positions.FirstOrDefaultAsync(x=>x.Id== id); 
+            if(dbposition == null)
+            {
+                return BadRequest();
+            }
+            return View(dbposition);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Position p, int? id)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return View();
+            //}
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Position? dbposition = await _db.Positions.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbposition == null)
+            {
+                return BadRequest();
+            }
+            dbposition.Name = p.Name;
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Position ?dbposition=await _db.Positions.FirstOrDefaultAsync(x=>x.Id== id);  
+            
+            if(dbposition == null)
+            {
+                return BadRequest();
+            }
+            if(dbposition.IsDeactive)
+            {
+                dbposition.IsDeactive = false;
+            }
+            else
+            {
+                dbposition.IsDeactive = true;
+            }
+
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
     }
 }
+
--- HotelProject/HotelProjectEntity/Entity/Position.cs
+++ HotelProject/HotelProjectEntity/Entity/Position.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace HotelProjectEntity.Entity
 {
     public class Position : BaseModel
     {
+        [Required(ErrorMessage ="Name is required!")]
         public string Name { get; set; }
         public List<Team> Teams { get; set; }
 
